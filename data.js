1d4c221891c59310f553b99e0cf707080acc3efe
kwancanoe <53270254+kwancanoe@users.noreply.github.com>
2024-09-15 18:53:55 +0700

    Updated

--- data.js
+++ data.js
@@ -10,10 +10,12 @@ const data = {
         { id: 2, parentId: 1, data: 'สินค้า' }
     ],
     level4: [
+        // Level 4 options under 'บริการ' (Services)
         { id: 1, parentId: 1, data: 'Cloud Vat เงินสด' },
         { id: 2, parentId: 1, data: 'Cloud Non Vat เงินสด' },
         { id: 3, parentId: 1, data: 'Cloud Vat เงินเชื่อ' },
         { id: 4, parentId: 1, data: 'Cloud Non Vat เงินเชื่อ' },
+        // Level 4 options under 'สินค้า' (Goods)
         { id: 5, parentId: 2, data: 'Notebook มายิง Ads (Vat  เงินสด)' },
         { id: 6, parentId: 2, data: 'Notebook มายิง Ads (Non Vat  เงินสด)' },
         { id: 7, parentId: 2, data: 'Notebook มายิง Ads (Vat  เงินเชื่อ)' },
@@ -24,23 +26,18 @@ const data = {
         { id: 12, parentId: 2, data: 'ซื้อกระดาษ A4 Non Vat เงินเชื่อ' }
     ],
     level5: [
+        // Level 5 options under 'Cloud Vat เงินสด'
         { id: 1, parentId: 1, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
-        { id: 2, parentId: 2, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
+        { id: 2, parentId: 1, data: 'ทรัพย์สิน' },
+        // Level 5 options under other subcategories
         { id: 3, parentId: 3, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
-        { id: 4, parentId: 4, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
-        { id: 5, parentId: 5, data: 'ทรัพย์สิน' },
-        { id: 6, parentId: 6, data: 'ทรัพย์สิน' },
-        { id: 7, parentId: 7, data: 'ทรัพย์สิน' },
-        { id: 8, parentId: 8, data: 'ทรัพย์สิน' },
-        { id: 9, parentId: 9, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
-        { id: 10, parentId: 10, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
-        { id: 11, parentId: 11, data: 'ค่าใช้จ่ายในการผลิด/บริการ' },
-        { id: 12, parentId: 12, data: 'ค่าใช้จ่ายในการผลิด/บริการ' }
+        { id: 4, parentId: 3, data: 'ทรัพย์สิน' }
     ],
     level6: [
+        // Level 6 options under 'ค่าใช้จ่ายในการผลิด/บริการ'
         { id: 1, parentId: 1, data: 'เงินสด/ธนาคาร (ซื้อสด)' },
         { id: 2, parentId: 1, data: 'เจ้าหนี้การค้า (ซื้อเชื่อ)' },
-        { id: 3, parentId: 1, data: 'ภาษีซื้อ' },
-        { id: 4, parentId: 1, data: 'ภาษีหัก ณ ที่จ่าย' }
+        { id: 3, parentId: 2, data: 'ภาษีซื้อ' },
+        { id: 4, parentId: 2, data: 'ภาษีหัก ณ ที่จ่าย' }
     ]
 };
